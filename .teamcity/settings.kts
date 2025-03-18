8cda587ce4c300483b96616f54f87daed0e57eb8
guycherno <72074365+guycherno@users.noreply.github.com>
2025-03-18 13:33:26 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -112,7 +112,7 @@ object BuildDockerImage : BuildType({
         }
         script {
             name = "Generate a Docker SBOM"
-            scriptContent = "/home/guyc/.scribe/bin/valint bom "+"mkjetbrains/todo-backend:%build.number%" + " -P %SCRIBE_TOKEN%" 
+            scriptContent = "/home/guyc/.scribe/bin/valint bom "+"mkjetbrains/todo-backend:%build.number% -vv" + " -P %SCRIBE_TOKEN%" 
         }    
     }
 })
