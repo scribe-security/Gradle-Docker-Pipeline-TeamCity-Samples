b92fbf5508a991d4e6bb9f87f95dcfae9257fb2a
guycherno <72074365+guycherno@users.noreply.github.com>
2025-03-21 11:07:40 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -96,9 +96,8 @@ object BuildApp : BuildType({
             name = "Generate a Source SBOM 1"
             
             scriptContent = """
-                export CURRENT=$(pwd)
                 az login --service-principal -u %env.APP_ID% -p %env.CLIENT_SECRET% --tenant %env.TENANT_ID%
-                /home/guyc/.scribe/bin/valint bom dir:$CURRENT -vv %env.SCRIBE_TOKEN% --product-key Team-City-Demo --product-version 1.0.4 -o attest --kms azurekms://guys-keys.vault.azure.net/code-signer-one
+                /home/guyc/.scribe/bin/valint bom dir:$(pwd) -vv %env.SCRIBE_TOKEN% --product-key Team-City-Demo --product-version 1.0.4 -o attest --kms azurekms://guys-keys.vault.azure.net/code-signer-one
                 
             """.trimIndent()
         }
