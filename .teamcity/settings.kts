f489e376c8572d20251fb5f8887b8c2bd3229349
guycherno <72074365+guycherno@users.noreply.github.com>
2025-05-25 22:18:52 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -92,23 +92,17 @@ object BuildApp : BuildType({
             tasks = "clean build"
             jdkHome = "%env.JDK_11%"
         }
-        script {
-            name = "Login To Azure Key Vault"
-            
-            scriptContent = """
-                echo '----------Key Vault---------------'
-                C:\Users\ContainerUser\Azure\CLI2\wbin\az login --service-principal -u %env.APP_ID% -p %env.CLIENT_SECRET% --tenant %env.TENANT_ID%
-                
-                
-            """.trimIndent()
-        }
-
-
+        
         script {
             name = "Generate a Source SBOM 1"
             
             scriptContent = """
-                echo '-----------Valint-----------------'
+                echo '-----------Az Login & Valint-----------------'
+                set AZURE_TENANT_ID=%env.TENANT_ID%
+                set AZURE_CLIENT_ID=%env.APP_ID%
+                set AZURE_CLIENT_SECRET=%env.CLIENT_SECRET%
+                if errorlevel 1 exit /b 1
+
                 C:\Users\ContainerUser\valint bom dir:$(pwd) -vv %env.SCRIBE_TOKEN% --product-key Team-City-Demo --product-version 1.0.4 -o attest --kms azurekms://guys-keys.vault.azure.net/code-signer-one
                 
             """.trimIndent()
